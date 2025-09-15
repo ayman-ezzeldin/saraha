723751094be413908fbda8d5fc254659f2f09697
Ayman ezz <aymanmae12@gmail.com>
2025-09-15 15:57:24 +0300

    add axios, cookies-next, store folder

--- src/i18n/request.ts
+++
@@ -1,9 +0,0 @@
-import {getRequestConfig} from 'next-intl/server';
-
-export default getRequestConfig(async ({locale}) => {
-  const resolvedLocale = locale ?? 'en'; 
-  return {
-    messages: (await import(`./messages/${resolvedLocale}.json`)).default,
-    locale: resolvedLocale
-  };
-});
